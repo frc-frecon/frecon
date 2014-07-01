1171590ac276e52c5528a518a51a309094b3bc67
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-01 11:58:40 -0400

    Load the migration extension in the database script.
    Now I can do stuff with migrations

--- db.rb
+++ db.rb
@@ -1,6 +1,9 @@
 require "logger"
 require "sequel"
 
+# Load the migration extension so we can do stuff with migrations.
+Sequel.extension :migration
+
 # This variable is global across all files and is accessible by the entire API.
 $db = Sequel.connect("sqlite://database/database.db", :logger => Logger.new("log/db.log"))
 
