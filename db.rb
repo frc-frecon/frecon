cf70d3bd54a6ff448ec76ce613def76755ecb828
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-01 11:59:13 -0400

    Check if we have outstanding migrations and migrate accordingly on db.rb

--- db.rb
+++ db.rb
@@ -7,6 +7,15 @@ Sequel.extension :migration
 # This variable is global across all files and is accessible by the entire API.
 $db = Sequel.connect("sqlite://database/database.db", :logger => Logger.new("log/db.log"))
 
+# If we have any outstanding migrations that have not been applied yet,
+unless Sequel::Migrator.is_current?($db, "database/migrations")
+	# Print out a warning.
+	warn "Warning: Outstanding migrations present, attempting to run them."
+
+	# Run them.
+	Sequel::Migrator.run($db, "database/migrations")
+end
+
 Dir.glob("models/*.rb").each do |file|
 	require_relative file
 end
