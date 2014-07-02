25f6e5724138f3086e729bd184c9e83e7b861b78
Sammidysam <sammidysam@gmail.com>
2014-07-02 11:24:09 -0400

    Fix ExtraData

--- models/extra_data.rb
+++ models/extra_data.rb
@@ -1,6 +1,6 @@
 require "sequel"
 
-class ExtraData < Sequel::Model
+class ExtraData < Sequel::Model(:extra_data)
 	def before_save
 		@values[:created_at] ||= DateTime.now
 		@values[:updated_at] = DateTime.now
