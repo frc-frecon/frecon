be7a0f683b219cfeeb48b763e06e4df41ec558f7
Sammidysam <sammidysam@gmail.com>
2014-07-03 20:12:13 -0400

    Plan for Position class

--- models/record.rb
+++ models/record.rb
@@ -1,6 +1,30 @@
 # Should be moved probably.
 class Position
+	# Mongo compatibility methods.
+	def self.demongoize(object)
+	end
+
+	def self.mongoize(object)
+	end
+
+	def self.evolve(object)
+	end
+
+	# Custom methods.
+	attr_reader :alliance, :number
+
+	def initialize(alliance, number)
+		@alliance, @number = alliance, number
+	end
 	
+	def is_red?
+	end
+
+	def is_blue?
+	end
+
+	alias_method :was_red?, :is_red?
+	alias_method :was_blue?, :is_blue?
 end
 
 class Record
