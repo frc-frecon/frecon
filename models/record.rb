0ab1af68a8530c54d72055f3cb8567afe0bb66a3
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-03 22:52:35 -0400

    Use RegExp for matching position.
    Now and instead, match against a simple regular expression that matches
    a number of potential cases. Data entry can now give us any value it
    wants to that conforms to a very simple standard (starts with "r" or
    "b", ends in a number) and we can likely parse it.
    
    It is, of course, preferred to give such simple values as "r1", "red1",
    or "red_1", but the system can account for that now.
    
    I also added a lot of comments. @Sammidysam was right, I like to think I
    *did* improve it.

--- models/record.rb
+++ models/record.rb
@@ -14,10 +14,41 @@ class Position
 	attr_reader :alliance, :number
 
 	def initialize(string)
-		@alliance = (string[0] == "r" ? :red : :blue)
-		@number = string[1].to_i
+		# Match `string' against the regular expression, described below.
+		#
+		# This regular expression matches all values for `string' where
+		# the first letter is either "r" or "b" (case-insensitive due to /i
+		# at the end of the regular expression) and the last one-or-more
+		# characters in the string are digits 0-9. Anything between those two
+		# that is either a letter or an underscore is not retained, but
+		# if other characters exist (e.g. spaces as of right now) `string'
+		# will not match.
+		#
+		# You can use any words you like if you have more than just
+		# "r<n>" or "b<n>", for example "red_2" matches just the same
+		# as "r2", or, just for fun, just the same as "royal______2".
+		#
+		# This behavior may change in the future.
+		match_data = string.match(/^([rb])[a-z\_]*([0-9]+)/i)
+
+		# Note: if matched at all, match_data[0] is the entire
+		# string that was matched, hence the indices that start
+		# at one.
+
+		# TODO: Make this smaller.
+		case match_data[1].downcase
+		when "r"
+			@alliance = :red
+		when "b"
+			@alliance = :blue
+		else
+			@alliance = :unknown
+		end
+
+		# TODO: Add error handling.
+		@number = match_data[2].to_i
 	end
-	
+
 	def is_red?
 		@alliance == :red
 	end
