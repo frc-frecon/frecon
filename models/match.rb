35bf22994461e7d85a3652de369a06b493d4b30d
Sammidysam <sammidysam@gmail.com>
2014-07-03 20:20:24 -0400

    Make primitive MatchNumber class

--- models/match.rb
+++ models/match.rb
@@ -1,6 +1,50 @@
 # Should probably be moved.
 class MatchNumber
-	
+	# MongoDB compatibility methods.
+	def self.demongoize(object)
+	end
+
+	def self.mongoize(object)
+	end
+
+	def self.evolve(object)
+	end
+
+	# Custom methods.
+	attr_reader :round, :number
+
+	def initialize(string)
+		@replay = false
+		@type = :qualification
+	end
+
+	def replay?
+		@replay
+	end
+
+	def practice?
+		@type == :practice
+	end
+
+	def qualification?
+		@type == :qualification
+	end
+
+	def quarterfinal?
+		@type == :quarterfinal
+	end
+
+	def semifinal?
+		@type == :semifinal
+	end
+
+	def final?
+		@type == :final
+	end
+
+	def elimination?
+		[:quarterfinal, :semifinal, :final].include? @type
+	end
 end
 
 class Match
