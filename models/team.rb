16099d0bb59ad6dda77eae4cab73f44ea2f52c31
Sammidysam <sammidysam@gmail.com>
2014-07-03 20:23:46 -0400

    Switch to Team number schema

--- models/team.rb
+++ models/team.rb
@@ -2,9 +2,13 @@ class Team
 	include Mongoid::Document
 	include Mongoid::Timestamps
 
+	field :_id, type: Integer
+
 	field :name, type: String
 	field :location, type: String
 
 	has_many :participations
 	has_many :records
+
+	validates :_id, uniqueness: true
 end
