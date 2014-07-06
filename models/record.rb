ef23da4ba5715d31da3492d35d807df6da00aa3e
Sammidysam <sammidysam@gmail.com>
2014-07-06 11:16:23 -0400

    Allow non-string argument to Position

--- models/record.rb
+++ models/record.rb
@@ -13,52 +13,64 @@ class Position
 	# Custom methods.
 	attr_reader :alliance, :number
 
-	def initialize(string)
-		# Match `string' against the regular expression, described below.
-		#
-		# This regular expression matches all values for `string' where
-		# the first letter is either "r" or "b" (case-insensitive due to /i
-		# at the end of the regular expression) and the last one-or-more
-		# characters in the string are digits 0-9. Anything between those two
-		# that is either a letter or an underscore is not retained, but
-		# if other characters exist (e.g. spaces as of right now) `string'
-		# will not match.
-		#
-		# You can use any words you like if you have more than just
-		# "r<n>" or "b<n>", for example "red_2" matches just the same
-		# as "r2", or, just for fun, just the same as "royal______2".
-		#
-		# This behavior may change in the future.
-		match_data = string.match(/^([rb])[a-z\_]*([0-9]+)/i)
+	def initialize(*args)
+		if args.length == 1
+			# Match `string' against the regular expression, described below.
+			#
+			# This regular expression matches all values for `string' where
+			# the first letter is either "r" or "b" (case-insensitive due to /i
+			# at the end of the regular expression) and the last one-or-more
+			# characters in the string are digits 0-9. Anything between those two
+			# that is either a letter or an underscore is not retained, but
+			# if other characters exist (e.g. spaces as of right now) `string'
+			# will not match.
+			#
+			# You can use any words you like if you have more than just
+			# "r<n>" or "b<n>", for example "red_2" matches just the same
+			# as "r2", or, just for fun, just the same as "royal______2".
+			#
+			# This behavior may change in the future.
+			match_data = args[0].match(/^([rb])[a-z\_]*([0-9]+)/i)
 
-		# Note: if matched at all, match_data[0] is the entire
-		# string that was matched, hence the indices that start
-		# at one.
+			# Note: if matched at all, match_data[0] is the entire
+			# string that was matched, hence the indices that start
+			# at one.
+			
+			@alliance = case match_data[1].downcase
+			            when "b"
+				            :blue
+			            when "r"
+				            :red
+			            else
+				            :unknown
+			            end
 
-		# TODO: Make this smaller.
-		case match_data[1].downcase
-		when "r"
-			@alliance = :red
-		when "b"
-			@alliance = :blue
+			position_number = match_data[2].to_i
+			raise ArgumentError, "position number must be in [1, 2, 3]" unless [1, 2, 3].include?(position_number)
+			
+			@number = position_number
+		elsif args.length == 2
+			@alliance = [:blue, :red].include?(args[0]) ? args[0] : :unknown
+
+			raise TypeError, "second argument must be an Integer" unless args[1].is_a?(Integer)
+			raise ArgumentError, "second argument must be in [1, 2, 3]" unless [1, 2, 3].include?(args[1])
+			
+			@number = args[1]
 		else
-			@alliance = :unknown
+			raise ArgumentError, "wrong number of arguments (#{args.length} for [1, 2])"
 		end
-
-		# TODO: Add error handling.
-		@number = match_data[2].to_i
-	end
-
-	def is_red?
-		@alliance == :red
 	end
 
 	def is_blue?
 		@alliance == :blue
 	end
 
-	alias_method :was_red?, :is_red?
+	def is_red?
+		@alliance == :red
+	end
+
 	alias_method :was_blue?, :is_blue?
+	alias_method :was_red?, :is_red?
 end
 
 class Record
