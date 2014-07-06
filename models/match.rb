b90927149216afa7254bf938a523dd38dc994757
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-06 19:11:02 -0400

    MatchNumber.initialize: Use match groups to populate variables.

--- models/match.rb
+++ models/match.rb
@@ -21,6 +21,30 @@ class MatchNumber
 		@replay_number = nil
 
 		match_data = string.match(/(p|q|qf|sf|f)([\d]+)?m([\d]+)(r)?([\d]+)?/i)
+
+		case match_data[1].downcase
+		when "p"
+			@type = :practice
+		when "q"
+			@type = :qualification
+		when "qf"
+			@type = :quarterfinal
+		when "sf"
+			@type = :semifinal
+		when "f"
+			@type = :final
+		end
+
+		if match_data[2]
+			@round = match_data[2].to_i
+		end
+
+		@number = match_data[3].to_i
+
+		if match_data[4] == "r"
+			@replay = true
+			@replay_number = match_data[5].to_i
+		end
 	end
 
 	def replay?
