d9ca7fde0774eb24424f24dfb1a66b5922f8cd5c
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-06 19:12:18 -0400

    MatchNumber.initialize: Add comments.

--- models/match.rb
+++ models/match.rb
@@ -20,8 +20,34 @@ class MatchNumber
 		@replay = false
 		@replay_number = nil
 
+		# Match `string' against the regular expression, described below.
+		#
+		# This regular expression matches all values for `string' where the first
+		# group of characters is one of either [ "p", "q", "qf", "sf", "f" ],
+		# which is parsed as the "type" of the match. This is followed by an
+		# "m" and a group of digits, which is parsed as the "number" of the match.
+		#
+		# In addition, one can specify a "round number" following the first group
+		# of characters such as in eliminations and finals. Often times, there
+		# are multiple so-called "rounds" in eliminations, and so the system will
+		# optionally capture that round.
+		#
+		# Also, one can specify a "replay number" following the match number.
+		# this is done by appending "r" and a group of digits which is the replay
+		# number.
+		#
+		# Below are listed the match groups and what they are:
+		#
+		# 1: Match type
+		# 2: Round number (optional)
+		# 3: Match number
+		# 4: Replay string (optional)
+		# 5: Replay number (required if #4 is supplied)
+		#
+		# This behavior may change in the future.
 		match_data = string.match(/(p|q|qf|sf|f)([\d]+)?m([\d]+)(r)?([\d]+)?/i)
 
+		# Parse the match type string
 		case match_data[1].downcase
 		when "p"
 			@type = :practice
@@ -35,12 +61,15 @@ class MatchNumber
 			@type = :final
 		end
 
+		# Parse the round number, if it is present
 		if match_data[2]
 			@round = match_data[2].to_i
 		end
 
+		# Parse the match number
 		@number = match_data[3].to_i
 
+		# Parse replay match groups
 		if match_data[4] == "r"
 			@replay = true
 			@replay_number = match_data[5].to_i
