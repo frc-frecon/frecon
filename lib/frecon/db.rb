e506931e40bc2d210a6c8d015ae8fd3ef1652bf8
Sammidysam <sammidysam@gmail.com>
2014-07-12 17:29:21 -0400

    Make database more library-like

--- lib/frecon/db.rb
+++ lib/frecon/db.rb
@@ -1,14 +1,20 @@
 require "logger"
 require "mongoid"
 
-Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
+module FReCon
+	class Database
+		def self.setup(environment = :development)
+			Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"), environment)
 
-Mongoid.logger.level = Logger::DEBUG
-Mongoid.logger = Logger.new($stdout)
+			Mongoid.logger.level = Logger::DEBUG
+			Mongoid.logger = Logger.new($stdout)
 
-Moped.logger.level = Logger::DEBUG
-Moped.logger = Logger.new($stdout)
+			Moped.logger.level = Logger::DEBUG
+			Moped.logger = Logger.new($stdout)
 
-Dir.glob(File.join(File.dirname(__FILE__), "models/*.rb")).each do |file|
-	require_relative file
+			Dir.glob(File.join(File.dirname(__FILE__), "models/*.rb")).each do |file|
+				require_relative file
+			end
+		end
+	end
 end
