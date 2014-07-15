db5a1810271dd60deab32f1156da33819ba1da5a
Sammidysam <sammidysam@gmail.com>
2014-07-15 14:40:40 -0400

    Bring IRB support back

--- lib/frecon/console.rb
+++ lib/frecon/console.rb
@@ -1,11 +1,23 @@
-require "pry"
-
 module FReCon
 	class Console
 		def self.start
 			FReCon::Database.setup(FReCon::Server.environment)
 
-			FReCon.pry
+			begin
+				raise Exception
+				require "pry"
+				
+				FReCon.pry
+			rescue Exception
+				require "irb"
+
+				IRB.setup nil
+				IRB.conf[:MAIN_CONTEXT] = IRB::Irb.new.context
+
+				require "irb/ext/multi-irb"
+
+				IRB.irb nil, FReCon
+			end
 		end
 	end
 end
