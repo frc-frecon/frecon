f41a4ca4eaa607c1c16a60157b56859385d3c468
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-17 19:11:35 -0400

    Hotfix another problemosesos

--- lib/frecon/controllers.rb
+++ lib/frecon/controllers.rb
@@ -2,5 +2,8 @@
 require "json"
 require "frecon/error_formatter"
 
+require "frecon/controllers/competitions_controller"
+require "frecon/controllers/matches_controller"
+require "frecon/controllers/records_controller"
 require "frecon/controllers/teams_controller"
 require "frecon/controllers/dump_controller"
