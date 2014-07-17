dc5b77ba6bab6137a22136d74d75613ffc7e6c47
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-17 12:31:04 -0400

    Some more work on Dumping
    It's a bit rudimentary, but it'll work for now.

--- lib/frecon/controllers/dump_controller.rb
+++ lib/frecon/controllers/dump_controller.rb
@@ -1,9 +1,14 @@
- require "json"
+require "json"
 
- module FReCon
+module FReCon
 	class DumpController
-		def self.get(params)
+		def self.full(params)
+			competitions = FReCon::Competition.all.to_a
+			teams = FReCon::Team.all.to_a
+			matches = FReCon::Match.all.to_a
+			records = FReCon::Record.all.to_a
 
+			[200, { "competitions" => competitions, "teams" => teams, "matches" => matches, "records" => records }.to_json]
 		end
 	end
 end
