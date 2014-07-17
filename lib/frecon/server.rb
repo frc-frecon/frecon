34c6f0ebc2bfb30dcfe524e7876f3c94e74256ef
Sammidysam <sammidysam@gmail.com>
2014-07-17 11:07:49 -0400

    Make all content type JSON

--- lib/frecon/server.rb
+++ lib/frecon/server.rb
@@ -6,6 +6,10 @@ module FReCon
 	class Server < Sinatra::Base
 		include FReCon::Routes
 
+		before do
+			content_type "application/json"
+		end
+
 		def self.start
 			FReCon::Database.setup(environment)
 			run!
