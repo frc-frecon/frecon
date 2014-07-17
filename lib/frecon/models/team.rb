090fc1ac6e27ba421a705f3fa2b35a145a7e90f6
Sammidysam <sammidysam@gmail.com>
2014-07-17 10:11:20 -0400

    Remove unnecessary methods

--- lib/frecon/models/team.rb
+++ lib/frecon/models/team.rb
@@ -17,40 +17,6 @@ module FReCon
 			find_by number: team_number
 		end
 
-		def self.name_good?(name)
-			false unless name
-			false unless name.is_a?(String)
-			true
-		end
-
-		def self.location_good?(location)
-			false unless location
-			false unless location.is_a?(String)
-			true
-		end
-
-		def self.number_good?(number)
-			false unless number
-			false unless number.is_a?(Integer)
-			true
-		end
-
-		def name_good?
-			FReCon::Team.name_good?(@name)
-		end
-
-		def number_good?
-			FReCon::Team.number_good?(@number)
-		end
-
-		def location_good?
-			FReCon::Team.location_good?(@location)
-		end
-
-		def to_h
-			{"name" => @name, "location" => @location, "number" => @number}
-		end
-
 		# alias_method works by default solely on instance
 		# methods, so change context to the metaclass of
 		# Team and do aliasing there.
