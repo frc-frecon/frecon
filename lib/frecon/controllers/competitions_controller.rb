317b3a800e739519bf1973d98fce8beb8dc8097d
Sammidysam <sammidysam@gmail.com>
2014-07-19 10:53:32 -0400

    Clean up CompetitionsController

--- lib/frecon/controllers/competitions_controller.rb
+++ lib/frecon/controllers/competitions_controller.rb
@@ -2,96 +2,14 @@ require "json"
 require "frecon/models"
 
 module FReCon
-	class CompetitionsController
-		def self.create(request, params)
-			# Rewind the request body (an IO object)
-			# in case someone else has already played
-			# through it.
-			request.body.rewind
-
-			begin
-				# Parse the POST data as a JSON hash
-				# (because that's what it is)
-				post_data = JSON.parse(request.body.read)
-			rescue JSON::ParserError => e
-				# If we have malformed JSON (JSON::ParserError is raised),
-				# escape out of the function
-				return [400, ErrorFormatter.format(e.message)]
-			end
-
-			@competition = Competition.new
-			@competition.attributes = post_data
-
-			if @competition.save
-				# Use to_json for now; we can filter it later.
-				[201, @competition.to_json]
-			else
-				[422, ErrorFormatter.format(@competition.errors.full_messages)]
-			end
-		end
-
-		def self.update(request, params)
-			return [400, "Must supply a competition id!"] unless params[:id]
-
-			# Rewind the request body (an IO object)
-			# in case someone else has already played
-			# through it.
-			request.body.rewind
-
-			begin
-				# Parse the POST data as a JSON hash
-				# (because that's what it is)
-				post_data = JSON.parse(request.body.read)
-			rescue JSON::ParserError => e
-				# If we have malformed JSON (JSON::ParserError is raised),
-				# escape out of the function
-				return [422, ErrorFormatter.format(e.message)]
-			end
-
-			@competition = Competition.find params[:id]
-
-			if @competition.nil?
-				return [404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
-			end
-
-			if @competition.update_attributes(post_data)
-				@competition.to_json
-			else
-				[422, ErrorFormatter.format(@competition.errors.full_messages)]
-			end
-		end
-
-		def self.delete(params)
-			@competition = Competition.find params[:id]
-
-			if @competition
-				if @competition.destroy
-					204
-				else
-					[422, ErrorFormatter.format(@competition.errors.full_messages)]
-				end
-			else
-				[404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
-			end
-		end
-
-		def self.show(params)
-			@competition = Competition.find params[:id]
-
-			if @competition
-				@competition.to_json
-			else
-				[404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
-			end
-		end
-
+	class CompetitionsController < Controller
 		def self.teams(params)
 			@competition = Competition.find params[:id]
 
 			if @competition
 				@competition.teams.to_json
 			else
-				[404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
+				[404, ErrorFormatter.format(could_not_find(params[:id]))]
 			end
 		end
 
@@ -101,7 +19,7 @@ module FReCon
 			if @competition
 				@competition.matches.to_json
 			else
-				[404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
+				[404, ErrorFormatter.format(could_not_find(params[:id]))]
 			end
 		end
 
@@ -111,16 +29,8 @@ module FReCon
 			if @competition
 				@competition.records.to_json
 			else
-				[404, ErrorFormatter.format("Could not find competition of id #{params[:id]}!")]
+				[404, ErrorFormatter.format(could_not_find(params[:id]))]
 			end
-		end
-
-		def self.index(params)
-			params.delete("_")
-
-			@competitions = params.empty? ? Competition.all : Competition.where(params)
-
-			@competitions.to_json
 		end
 	end
 end
