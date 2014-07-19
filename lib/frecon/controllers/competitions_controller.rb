cada9a84eac3f7cc77a2e9e322b568ce06239d55
Sammidysam <sammidysam@gmail.com>
2014-07-19 11:11:38 -0400

    Use show_attribute in CompetitionsController

--- lib/frecon/controllers/competitions_controller.rb
+++ lib/frecon/controllers/competitions_controller.rb
@@ -4,33 +4,15 @@ require "frecon/models"
 module FReCon
 	class CompetitionsController < Controller
 		def self.teams(params)
-			@competition = Competition.find params[:id]
-
-			if @competition
-				@competition.teams.to_json
-			else
-				[404, ErrorFormatter.format(could_not_find(params[:id]))]
-			end
+			show_attribute params, :teams
 		end
 
 		def self.matches(params)
-			@competition = Competition.find params[:id]
-
-			if @competition
-				@competition.matches.to_json
-			else
-				[404, ErrorFormatter.format(could_not_find(params[:id]))]
-			end
+			show_attribute params, :matches
 		end
 
 		def self.records(params)
-			@competition = Competition.find params[:id]
-
-			if @competition
-				@competition.records.to_json
-			else
-				[404, ErrorFormatter.format(could_not_find(params[:id]))]
-			end
+			show_attribute params, :records
 		end
 	end
 end
