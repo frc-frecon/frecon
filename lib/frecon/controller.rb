5fe80d4dfeea49248b12b2267221ed6c29441e71
Sam Craig <sammidysam@gmail.com>
2014-07-22 21:17:20 -0400

    Remove unneeded return statement

--- lib/frecon/controller.rb
+++ lib/frecon/controller.rb
@@ -34,7 +34,7 @@ module FReCon
 				return [400, ErrorFormatter.format(e.message)]
 			end
 
-			return [422, ErrorFormatter.format("Must pass a JSON object!")] if post_data.is_a?(Array)
+			[422, ErrorFormatter.format("Must pass a JSON object!")] if post_data.is_a?(Array)
 		end
 		
 		def self.create(request, params)
