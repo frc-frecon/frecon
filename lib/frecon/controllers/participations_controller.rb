d5a6c67db5e9a1b1d7266c45405af25495bafa0f
Sammidysam <sammidysam@gmail.com>
2014-08-16 21:19:56 -0400

    Allow passing team number to create a new Participation

--- lib/frecon/controllers/participations_controller.rb
+++ lib/frecon/controllers/participations_controller.rb
@@ -1,5 +1,30 @@
 module FReCon
 	class ParticipationsController < Controller
+		def self.create(request, params)
+			post_data = process_request request
+			return post_data if post_data.is_an?(Array)
+			
+			# Convert team number to team_id.
+			if post_data["team_number"]
+				unless (team = Team.number post_data["team_number"]).nil?
+					post_data["team_id"] = team.id
+
+					post_data.delete("team_number")
+				else
+					return [404, ErrorFormatter.format(could_not_find(post_data["team_number"], "number", "team"))]
+				end
+			end
+
+			@model = model.new
+			@model.attributes = post_data
+
+			if @model.save
+				[201, @model.to_json]
+			else
+				[422, ErrorFormatter.format(@model.errors.full_messages)]
+			end
+		end
+		
 		def self.competition(params)
 			show_attribute params, :competition
 		end
