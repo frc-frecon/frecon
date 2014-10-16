221f1b5296cc76029ec5c1c0b8d2b9035d606e86
Sammidysam <sammidysam@gmail.com>
2014-10-16 19:02:41 -0400

    Add a name field
    Robots should all have names

--- lib/frecon/models/robot.rb
+++ lib/frecon/models/robot.rb
@@ -2,9 +2,7 @@ require "frecon/model"
 
 module FReCon
 	class Robot < Model
-		# Pre-scouting changes depending on the team doing it,
-		# so we are leaving the defined fields blank.
-		# Store your own information using dynamic attributes.
+		field :name, type: String
 
 		belongs_to :competition
 		belongs_to :team
