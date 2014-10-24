77cfb3e8965d0821af0153d54b6241c6cb2f5fea
Sammidysam <sammidysam@gmail.com>
2014-10-23 20:14:18 -0400

    Ensure that belongs_to validations are correct

--- lib/frecon/model.rb
+++ lib/frecon/model.rb
@@ -6,8 +6,21 @@ module FReCon
 			child.class_eval do
 				include Mongoid::Document
 				include Mongoid::Timestamps
-				include Mongoid::Attributes::Dynamic		
+				include Mongoid::Attributes::Dynamic
+
+				validate :no_invalid_relations
 			end
+		end
+
+		def no_invalid_relations
+			attributes.keys.select { |attribute| attribute.end_with?("_id") && attribute != "_id" }.each do |relation|
+				model = "FReCon::".concat(relation.gsub(/_id\Z/, "").capitalize).constantize
+				errors.add(relation.to_sym, "is invalid") if relation_invalid(model, send(relation))
+			end
+		end
+
+		def relation_invalid(class_constant, id)
+			class_constant.find_by(id: id).nil?
 		end
 	end
 end
