349114eac98c92dd8f12cc77572e553aeb03fa1e
Kristofer Rye <kristofer.rye@gmail.com>
2014-11-07 18:01:13 -0600

    Add MIT licensure notice to all of the files in the gem.

--- lib/frecon/controllers.rb
+++ lib/frecon/controllers.rb
@@ -1,4 +1,12 @@
-# Libraries that all controllers require.
+# lib/frecon/controllers.rb
+#
+# Copyright (C) 2014 Christopher Cooper, Sam Craig, Tiger Huang, Vincent Mai, Sam Mercier, and Kristofer Rye
+#
+# This file is part of FReCon, an API for scouting at FRC Competitions, which is
+# licensed under the MIT license.  You should have received a copy of the MIT
+# license with this program.  If not, please see
+# <http://opensource.org/licenses/MIT>.
+
 require "json"
 require "frecon/error_formatter"
 
