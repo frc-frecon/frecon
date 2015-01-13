2eefab06e7e01ab7df7037d5e2a122920c4d59df
Kristofer Rye <kristofer.rye@gmail.com>
2015-01-12 20:56:17 -0600

    Some work on the spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,35 @@
 require "logger"
 require "mongoid"
 
-require "frecon"
+# require "database_cleaner"
 
-Mongoid.load!(File.join(File.dirname(__FILE__), "..", "lib", "frecon", "mongoid.yml"), :test)
+require "securerandom"
+
+require "frecon/database"
+
+FReCon::Database.setup(:test)
+
+database_cleaner_good = false
+
+# This code is deferred because of an upstream issue with
+# DatabaseCleaner--says Mongoid can't use truncation but
+# lists truncation as an available ORM strategy thing.
+if database_cleaner_good
+	RSpec.configure do |config|
+		config.before(:suite) do
+			DatabaseCleaner[:mongoid].strategy = :truncation
+			DatabaseCleaner.clean_with(:truncation)
+		end
+
+		config.before(:each) do
+			DatabaseCleaner.start
+		end
+
+		config.after(:each) do
+			DatabaseCleaner.clean
+		end
+	end
+end
 
 RSpec::Matchers.define :be_a do |expected|
 	match do |actual|
