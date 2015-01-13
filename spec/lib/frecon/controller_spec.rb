1a9c2a72d19fcc57fece89311e602cbe21088ba6
Kristofer Rye <kristofer.rye@gmail.com>
2015-01-12 20:55:37 -0600

    Fix updated RSpec dependency stuff and such.

--- spec/lib/frecon/controller_spec.rb
+++ spec/lib/frecon/controller_spec.rb
@@ -1,15 +1,17 @@
 require "spec_helper"
 
+require "frecon/controller"
+
 describe FReCon::Controller do
 	[:model_name, :model, :could_not_find, :process_request].each do |method_name|
 		it "has a method #{method_name.to_s}" do
-			expect(FReCon::Controller.respond_to?(method_name)).to be_true
+			expect(FReCon::Controller.respond_to?(method_name)).to be(true)
 		end
 	end
 
 	[:create, :update, :delete, :show, :index, :show_attribute, :team_number_to_team_id].each do |method_name|
 		it "has a method #{method_name.to_s} <used for response>" do
-			expect(FReCon::Controller.respond_to?(method_name)).to be_true
+			expect(FReCon::Controller.respond_to?(method_name)).to be(true)
 		end
 	end
 
