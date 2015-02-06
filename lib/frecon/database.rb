ac44e13fc296aa2c6548889d1f963d00c64db046
Kristofer Rye <kristofer.rye@gmail.com>
2015-02-06 13:16:41 -0500

    Remove database log messages.

--- lib/frecon/database.rb
+++ lib/frecon/database.rb
@@ -27,12 +27,6 @@ module FReCon
 			else
 				Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"), environment)
 			end
-			
-			Mongoid.logger.level = Logger::DEBUG
-			Mongoid.logger = Logger.new($stdout)
-
-			Moped.logger.level = Logger::DEBUG
-			Moped.logger = Logger.new($stdout)
 		end
 	end
 end
