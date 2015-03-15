11e43050d8e1821edf44cb595c5364115f7d2a44
Sammidysam <sammidysam@gmail.com>
2015-03-15 14:54:30 -0400

    Scraper scaffolding
    Does this match what we thought was good, @rye?

--- lib/frecon/scraper.rb
+++ lib/frecon/scraper.rb
@@ -6,3 +6,27 @@
 # licensed under the MIT license.  You should have received a copy of the MIT
 # license with this program.  If not, please see
 # <http://opensource.org/licenses/MIT>.
+
+module FReCon
+	# The default scraper scrapes other FReCon instances.
+	# To scrape a different source, a descendant scraper should be used.
+	class Scraper
+		def initialize(base_url)
+			@base_url = base_url
+		end
+
+		# Reads and imports a data string.
+		# Determines what to do with information in the `context` hash.
+		def read(data, context = {})
+			
+		end
+
+		# If no arguments are passed, will import the whole other database.
+		# If only one argument is passed, will import all of that model.
+		# If two arguments are passed, will import the models that match
+		# the query params.
+		def get(model = nil, query = {})
+			
+		end
+	end
+end
