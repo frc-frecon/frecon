8b4de677604da49aa1c17e90cb86c47b6c4dd2cd
Sam Craig <sammidysam@gmail.com>
2015-03-16 19:40:10 -0400

    Account for us not using an ID hash anymore

--- lib/frecon/controller.rb
+++ lib/frecon/controller.rb
@@ -186,7 +186,7 @@ module FReCon
 					post_data.delete("competition_name")
 
 					post_data
-				elsif post_data["competition"] && post_data["competition"]["_id"] && post_data["competition"]["_id"]["$oid"] && (competition = Competition.find_by(id: post_data["competition"]["_id"]["$oid"]))
+				elsif post_data["competition"] && post_data["competition"]["_id"] && (competition = Competition.find_by(id: post_data["competition"]["_id"]))
 					# Try to set the match to the already existing match.
 					match = competition.matches.find_by number: post_data["match_number"]
 
