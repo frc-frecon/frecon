e2bdcce32e4920fd9be7e761b2c01d1e30ad85a4
Kristofer Rye <kristofer.rye@gmail.com>
2015-06-29 17:02:06 -0500

    Record: Remove old associations with matches and teams.

--- lib/frecon/models/record.rb
+++ lib/frecon/models/record.rb
@@ -15,9 +15,6 @@ module FReCon
 		field :notes, type: String
 		field :position, type: Position
 
-		belongs_to :match
-		belongs_to :team
-
 		validates :position, :match_id, :team_id, presence: true
 	end
 end
