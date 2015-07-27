bfd14ea980741dd7b35331ea31e59111062c5692
Kristofer Rye <kristofer.rye@gmail.com>
2015-07-27 16:23:43 -0500

    spec: Get rid of the old stuff.
    I've got a better handle on writing test suites, so I'm going to do it
    more efficiently here.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,17 +4,3 @@ require "mongoid"
 require "securerandom"
 
 require "frecon/database"
-
-FReCon::Database.setup(:test)
-
-RSpec::Matchers.define :be_a do |expected|
-	match do |actual|
-		actual.class == expected
-	end
-end
-
-RSpec::Matchers.define :include do |expected|
-	match do |actual|
-		actual.include?(expected)
-	end
-end
