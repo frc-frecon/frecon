de37d6fb2fdacdfc11f8b785026b1b345ab480ca
Kristofer Rye <kristofer.rye@gmail.com>
2015-07-28 16:40:32 -0500

    console_spec: Remove IRB simulator, now just test pry usage.

--- spec/lib/frecon/console_spec.rb
+++ spec/lib/frecon/console_spec.rb
@@ -14,30 +14,14 @@ describe FReCon::Console do
 			FReCon::Console.start
 		end
 
-		context "with pry simulated as missing" do
-			it "runs IRB" do
-				# Stub require to raise LoadError when the given file argument
-				# is "pry".
-				allow(FReCon::Console).to receive(:require) do |file|
-					if file == "pry"
-						raise LoadError, "cannot load such file -- #{file}"
-					else
-						require file
-					end
-				end
+		it "uses pry" do
+			allow(FReCon::Console).to receive(:require)
+			allow(FReCon).to receive(:pry)
 
-				expect(FReCon::Console).to receive(:require).with("pry").ordered
-				expect(FReCon::Console).to receive(:require).with("irb").ordered
+			expect(FReCon::Console).to receive(:require).with("pry").ordered
+			expect(FReCon).to receive(:pry).ordered
 
-				require "irb"
-
-				allow(IRB).to receive(:conf) { {} }
-
-				expect(IRB).to receive(:setup).with(nil).ordered
-				expect(IRB).to receive(:conf).ordered
-
-				FReCon::Console.start
-			end
+			FReCon::Console.start
 		end
 	end
 end
