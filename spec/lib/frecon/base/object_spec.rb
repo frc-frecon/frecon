242b75722c3c18e39445ac9e5f159408619da3f3
Kristofer Rye <kristofer.rye@gmail.com>
2015-08-06 13:00:27 -0500

    Instantiate methods for testing rather than getting them repeatedly.

--- spec/lib/frecon/base/object_spec.rb
+++ spec/lib/frecon/base/object_spec.rb
@@ -4,12 +4,15 @@ require "frecon/base/object"
 
 describe Object do
 	it "has a method :is_an? which is an alias to :is_a?" do
+		is_a = Object.method(:is_a?)
+		is_an = Object.method(:is_an?)
+
 		case RUBY_VERSION
 		when "2.3.0"
-			expect(Object.method(:is_an?).original_name)
-				.to eq(Object.method(:is_a?).original_name)
+			expect(is_an.original_name)
+				.to eq(is_a.original_name)
 		else
-			expect(Object.method(:is_an?)).to eq(Object.method(:is_a?))
+			expect(is_an).to eq(is_a)
 		end
 	end
 end
