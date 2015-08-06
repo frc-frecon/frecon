2b643c63fa7b473eb08661f19e9cbc92e2530122
Kristofer Rye <kristofer.rye@gmail.com>
2015-08-06 12:57:37 -0500

    Add case for RUBY_VERSION == "2.3.0" when checking method aliases.

--- spec/lib/frecon/base/object_spec.rb
+++ spec/lib/frecon/base/object_spec.rb
@@ -4,6 +4,12 @@ require "frecon/base/object"
 
 describe Object do
 	it "has a method :is_an? which is an alias to :is_a?" do
-		expect(Object.method(:is_an?)).to eq(Object.method(:is_a?))
+		case RUBY_VERSION
+		when "2.3.0"
+			expect(Object.method(:is_an?).original_name)
+				.to eq(Object.method(:is_a?).original_name)
+		else
+			expect(Object.method(:is_an?)).to eq(Object.method(:is_a?))
+		end
 	end
 end
