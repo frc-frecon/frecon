fd6c90ffd66810104a9a25c1c95fd7fd2cb2b926
Kristofer Rye <kristofer.rye@gmail.com>
2015-08-23 16:15:00 -0500

    Add an empty Configuration class.

---
+++ lib/frecon/configuration.rb
@@ -0,0 +1,13 @@
+# lib/frecon/console.rb
+#
+# Copyright (C) 2015 Christopher Cooper, Sam Craig, Tiger Huang, Vincent Mai, Sam Mercier, and Kristofer Rye
+#
+# This file is part of FReCon, an API for scouting at FRC Competitions, which is
+# licensed under the MIT license.  You should have received a copy of the MIT
+# license with this program.  If not, please see
+# <http://opensource.org/licenses/MIT>.
+
+module FReCon
+	class Configuration
+	end
+end
