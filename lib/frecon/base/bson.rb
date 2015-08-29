d80166973e68f5e2259a7af107051e0e7658c7de
Kristofer Rye <kristofer.rye@gmail.com>
2015-08-29 15:19:53 -0500

    Document BSON::ObjectId#as_json.

--- lib/frecon/base/bson.rb
+++ lib/frecon/base/bson.rb
@@ -11,6 +11,12 @@ module BSON
 	# Public: A monkey-patch for BSON::ObjectId which introduces a #as_json
 	# method.
 	class ObjectId
+		# Public: Get produce a JSON representation of this ObjectId.
+		#
+		# Since we don't want to produce a JSON Object for every ID, this method
+		# instead just returns the String _id value within this object.
+		#
+		# Returns a String containing the value of this ObjectId.
 		def as_json(*args)
 			to_s
 		end
