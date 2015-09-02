84267046d814af388c62db1f4d315f19a943be64
Kristofer Rye <kristofer.rye@gmail.com>
2015-09-02 16:36:44 -0500

    Change spec from pending to failing to test Travis' response.

--- spec/lib/frecon/base/bson_spec.rb
+++ spec/lib/frecon/base/bson_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 
 describe BSON::ObjectId do
 	describe "#as_json" do
-		it "takes no arguments and returns the string representation of the ID"
+		it "takes no arguments and returns the string representation of the ID" do
+			fail
+		end
 	end
 end
