1c642b64b86cf4fc8358e221c26f5b59f6437804
Kristofer Rye <kristofer.rye@gmail.com>
2015-12-05 13:02:18 -0600

    spec_helper: Start CodeClimate::TestReporter
    [GitHub] This commit, and others in the branch resolve #83.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ require "frecon"
 # Initialization code for Coveralls.io
 require "coveralls"
 Coveralls.wear!
+
+# Initialization code for CodeClimate
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
