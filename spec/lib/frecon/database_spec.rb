ddf1044359156246eee32e3534872a5b6b45fa34
Kristofer Rye <kristofer.rye@gmail.com>
2016-04-17 11:01:12 -0500

    Database: Start adding specs

--- spec/lib/frecon/database_spec.rb
+++ spec/lib/frecon/database_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 
+require 'frecon/database'
+
+describe FReCon::Database do
+	describe '.setup!' do
+		it 'calls Mongoid.load! with a String (filename) and a Symbol' do
+			allow(Mongoid).to receive(:load!).with(String, Symbol)
+			expect(Mongoid).to receive(:load!).with(String, Symbol)
+
+			FReCon::Database.setup!
+		end
+	end
+end
