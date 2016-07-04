9fabbfa0aefc67d3a1b18df43899cf09424bba77
Kristofer Rye <kristofer.rye@gmail.com>
2016-07-04 14:08:36 -0500

    frecon/controller: Remove unused dependencies and add JSON dependency
    We use neither frecon/base/environment nor frecon/base/variables, so we should just remove these.  We also cannot expect frecon/base/bson to provide JSON, so we load that.

--- lib/frecon/controller.rb
+++ lib/frecon/controller.rb
@@ -7,10 +7,10 @@
 # license with this program.  If not, please see
 # <http://opensource.org/licenses/MIT>.
 
+require 'json'
+
 require 'frecon/base/bson'
-require 'frecon/base/environment'
 require 'frecon/base/object'
-require 'frecon/base/variables'
 
 module FReCon
 	# Public: A base class to represent a controller.
