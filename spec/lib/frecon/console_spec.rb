cec078e1650dbe9bb433bd90cf15fec82a2bad08
Kristofer Rye <kristofer.rye@gmail.com>
2016-07-06 13:26:16 -0500

    Start working on requirement specs
    Not sure if I like them, but more examples is nice

--- spec/lib/frecon/console_spec.rb
+++ spec/lib/frecon/console_spec.rb
@@ -4,10 +4,17 @@ require 'frecon/console'
 
 describe FReCon::Console do
 	describe '.start' do
-		it 'takes keyword arguments, starts the database, and calls FReCon.pry' do
+		it 'calls FReCon::Database.setup!' do
 			allow(FReCon::Database).to receive(:setup!)
 			expect(FReCon::Database).to receive(:setup!)
 
+			allow(FReCon).to receive(:require).with('pry')
+			allow(FReCon).to receive(:pry)
+
+			FReCon::Console.start
+		end
+
+		it 'calls FReCon.pry' do
 			allow(FReCon).to receive(:pry)
 			expect(FReCon).to receive(:pry)
 
--- spec/lib/frecon_spec.rb
+++ spec/lib/frecon_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe 'lib/frecon.rb' do
+
+	it 'can be required with no error' do
+		expect { require 'frecon' }.not_to raise_error
+	end
+
+end
