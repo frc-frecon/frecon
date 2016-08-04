a3894bcd3c1db63af4925164a981cded2bcc2f70
Kristofer Rye <kristofer.rye@gmail.com>
2016-08-04 09:59:44 -0500

    SimpleCov: Move setup code to spec_helper and delete .simplecov
    I was getting some loading errors with that, so I figured I might as well fold it into the spec_helper since that's the only context it's used within.
    
    [GitHub] This commit resolves #118.

--- .simplecov
+++
@@ -1,9 +0,0 @@
-# -*- mode: ruby -*-
-
-require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
-SimpleCov.start do
-	add_filter '.bundle/'
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
 require 'rspec'
 
-# Initialization code for Coveralls.io
-require 'coveralls'
-Coveralls.wear!
-
 # Initialization code for CodeClimate
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
+
+# Set up SimpleCov
+SimpleCov.start do
+	formatter SimpleCov::Formatter::MultiFormatter.new([
+		SimpleCov::Formatter::HTMLFormatter,
+		CodeClimate::TestReporter::Formatter
+	])
+end
