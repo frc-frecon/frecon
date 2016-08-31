37b409751338617f27c8878605ca7622ece2064f
Kristofer Rye <kristofer.rye@gmail.com>
2016-08-31 12:31:21 -0500

    FReCon spec: Remove type expectation
    We don't need to have this expectation here.  The only behavior that
    should be tested by variables_spec is that the requirement of the file
    results in the loading of those two constants, since that is the use
    case elsewhere internally.

--- spec/lib/frecon/base/variables_spec.rb
+++ spec/lib/frecon/base/variables_spec.rb
@@ -5,7 +5,6 @@ require 'frecon/base/variables'
 describe FReCon do
 	it 'has a version' do
 		expect(FReCon.const_get(:VERSION)).to_not be_nil
-		expect(FReCon.const_get(:VERSION)).to be_a FReCon::Version
 	end
 
 	it 'has an environment' do
